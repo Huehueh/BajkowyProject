f5ed63c527208580678ec8dc51adf11b4c87af6f
Huehueh <kinga.karczewska67@gmail.com>
2021-02-20 17:30:46 +0100

    reminder to put rfid card

--- bajkowy-functions.js
+++ bajkowy-functions.js
@@ -2,8 +2,10 @@
 
 async function ReadRfid()
 {
-     let r = await fetch('hue/read_rfid', {method: "GET"});
-     var rfid_text = document.getElementById("rfid_response");
-     rfid_text.innerHTML = await r.text();
-     console.log(r);
+    var rfid_text = document.getElementById("rfid_response");
+    rfid_text.innerHTML = "Przyłóż kartę RFID";
+
+    let r = await fetch('hue/read_rfid', {method: "GET"});
+    rfid_text.innerHTML = await r.text();
+    console.log(r);
 }
