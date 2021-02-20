6b75307b572ed4142ee5abdc2a14e334ec93d4a1
Huehueh <kinga.karczewska67@gmail.com>
2021-02-20 10:10:46 +0100

    uwsgi test

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from rfid import RFIDReader
 
 app = Flask(__name__)
 
-@app.route("/read_rfid", methods=['GET'])
+@app.route("/hue/read_rfid", methods=['GET'])
 def rfid():
     reader = RFIDReader()
     return reader.read_rfid()
+
+@app.route("/hue")
+def render():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- bajkowy-functions.js
+++ bajkowy-functions.js
@@ -2,9 +2,9 @@
 
 async function ReadRfid()
 {
-     hostname_ = location.protocol + "//" + location.hostname + ":" + location.port;
-     let path = hostname_ + "/read_rfid";
-     let r = await fetch('/read_rfid', {method: "GET"});
+//     hostname_ = location.protocol + "//" + location.hostname + ":" + location.port;
+//     let path = hostname_ + "/read_rfid";
+     let r = await fetch('hue/read_rfid', {method: "GET"});
      var rfid_text = document.getElementById("rfid_response");
      console.log(r);
      rfid_text.value = r;
