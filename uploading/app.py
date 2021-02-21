35a326ee3e6f98336523ba98c9fb7a316c869d48
Huehueh <kinga.karczewska67@gmail.com>
2021-02-21 21:55:24 +0100

    uploading sound files - part 2

--- uploading/app.py
+++ uploading/app.py
@@ -1,17 +1,15 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, send_from_directory
 from storytelling.rfid import RFIDReader
 from storytelling.config import ConfigReader
-
-ALLOWED_EXTENSIONS = {'mp3'}
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+import os
 
 app = Flask(__name__)
 configReader = ConfigReader()
-app['UPLOAD_FOLDER'] = configReader.get_sound_directory()
+app.config['UPLOAD_FOLDER'] = configReader.get_sound_directory()
+app.config['UPLOAD_EXTENSIONS'] = ['.mp3']
+app.config['UPLOAD_PATH'] = 'uploads'
 reader = None
+id = None
 
 def init_rfid_reader():
     global reader
@@ -20,22 +18,24 @@ def init_rfid_reader():
 
 @app.route("/read_rfid", methods=['GET'])
 def rfid():
+    global id
     init_rfid_reader()
-    return reader.read_id()
-
-# @app.post("/upload_sound")
-# def upload_sound(sound):
-#     try:
-#         sound.file.seek(0)
+    id = reader.read_id()
+    return id
 
 @app.route('/', methods='POST')
 def upload_sound():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         print("PliczeK", uploaded_file.filename)
-        uploaded_file.save(uploaded_file.filename)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH', uploaded_file.filename))
     return redirect(url_for('index'))
-    # configReader.add_song(id, soundname)
+    #
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    configReader.add_song(id, filename)
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
