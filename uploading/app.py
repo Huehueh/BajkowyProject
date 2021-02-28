04931fd2c860e8da8e663a796402bd6ed7532f5f
Huehueh <kinga.karczewska67@gmail.com>
2021-02-28 19:29:29 +0100

    Uploading songs & saving config working

--- uploading/app.py
+++ uploading/app.py
@@ -10,7 +10,6 @@ app.config['UPLOAD_EXTENSIONS'] = ['.mp3']
 app.config['UPLOAD_PATH'] = 'uploads'
 app.config['MAX_CONTENT_PATH'] = 100000000
 reader = None
-id = None
 
 def init_rfid_reader():
     global reader
@@ -18,23 +17,22 @@ def init_rfid_reader():
         reader = RFIDReader()
 
 @app.route("/read_rfid", methods=['GET'])
-def rfid():
-    global id
+def read_rfid():
     init_rfid_reader()
-    id = reader.read_id()
-    return id
+    return reader.read_id()
 
-@app.route('/upload_file', methods=['GET', 'POST'])
+@app.route('/upload_file', methods=['POST'])
 def upload_sound():
-    if request.method == 'POST':
-        uploaded_file = request.files['file']
-        print("upload folder", app.config['UPLOAD_FOLDER'])
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-        print("Zapisuje plik", filepath)
-        uploaded_file.save(filepath)
-        print(f"Zapisuje config {id} - {uploaded_file.filename}")
-        configReader.add_song(id, uploaded_file.filename)
-    return f"wrzucono {uploaded_file.filename}"
+    uploaded_file = request.files['file']
+
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+    print("Zapisuje plik", filepath)
+    uploaded_file.save(filepath)
+
+    id = request.form["rfid_response"]
+    print(f"Zapisuje config {id} - {uploaded_file.filename}")
+    configReader.add_song(id, uploaded_file.filename)
+    return f"Wrzucono {id} {uploaded_file.filename}"
 
 
 if __name__ == "__main__":
--- uploading/bajkowy-functions.js
+++ uploading/bajkowy-functions.js
@@ -3,9 +3,9 @@
 async function ReadRfid()
 {
     var rfid_text = document.getElementById("rfid_response");
-    rfid_text.innerHTML = "Przyłóż kartę RFID";
+    rfid_text.value = "Przyłóż kartę RFID";
 
     let r = await fetch('hue/read_rfid', {method: "GET"});
-    rfid_text.innerHTML = await r.text();
+    rfid_text.value = await r.text();
     console.log(r);
 }
