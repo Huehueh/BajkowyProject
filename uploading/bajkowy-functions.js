fda0bfcd0efdba9fe32e866ce54601f1597191e2
Huehueh <kinga.karczewska67@gmail.com>
2021-02-28 21:47:44 +0100

    validating rfid in JS

--- uploading/bajkowy-functions.js
+++ uploading/bajkowy-functions.js
@@ -1,9 +1,10 @@
 
+rfid_await_message = "Przyłóż kartę RFID";
 
 async function ReadRfid()
 {
     var rfid_text = document.getElementById("rfid_response");
-    rfid_text.value = "Przyłóż kartę RFID";
+    rfid_text.value = rfid_await_message;
 
     let r = await fetch('hue/read_rfid', {method: "GET"});
     rfid_text.value = await r.text();
@@ -18,15 +19,21 @@ window.onload = async function() {
 
   form.onsubmit = async function(event) {
       var data = new FormData(form);
-      event.preventDefault();
-      loader.style.display = "block";
-      sound_name.innerHTML = "";
+      if (data.rfid_response != "" && data.rfid_response != rfid_await_message)
+      {
+          event.preventDefault();
+          loader.style.display = "block";
+          sound_name.innerHTML = "";
 
-      let ret = await fetch('/hue/upload_file', {method: "POST", body: data});
-      sound_name.innerHTML = await ret.text();
-      loader.style.display = "none";
-      console.log(ret);
-
+          let ret = await fetch('/hue/upload_file', {method: "POST", body: data});
+          sound_name.innerHTML = await ret.text();
+          loader.style.display = "none";
+          console.log(ret);
+      }
+      else
+      {
+        sound_name.innerHTML = "Niepoprawny numer RFID";
+      }
       return false;
     }
 }
